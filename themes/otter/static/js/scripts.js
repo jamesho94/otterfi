ff6de46bc4115d0fa806c059b857ebaaf960d5ed
James Ho <jameshoaz@gmail.com>
2017-10-09 23:51:55 +0000

    Created color variables

---
+++ themes/otter/static/js/scripts.js
@@ -0,0 +1,13 @@
+var profitColor = "#3498db"
+var budgetingColor = "#1abc9c"
+var debtColor = 
+
+function hover(hometile){
+    hometile.style.backgroundColor = "#3498db";
+    hometile.style.boxShadow = "";
+}
+
+function static(hometile){
+    hometile.style.backgroundColor = "#F8FDFE";
+    hometile.style.boxShadow = "10px 10px 5px #AEB1B1";
+}
